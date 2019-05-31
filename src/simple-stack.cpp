1089caf124f982e2f22911caaab458fcafb6e9e6
LucasPerri <lucas.perri@philips.edu.ar>
2019-05-31 18:04:18 -0300

    Node class definition

--- src/simple-stack.cpp
+++ src/simple-stack.cpp
@@ -1,4 +1,25 @@
 class Node;
 class Stack;
 
+class Node
+{
+private:
+    // Value stored in the node
+    int _value;
+    // Pointer to the next node 
+    Node *_next;
+public:
+    // Returns the value stored in the node
+    const int value(){ return this->_value; }
+    // Returns a pointer to the next node
+    const Node* next(){ return this->_next; }
+
+    Node(int value, Node *next=nullptr)
+    {
+        this->_value = value;
+        this->_next = next;
+    }
+    ~Node();
+};
+
 int main(){}
